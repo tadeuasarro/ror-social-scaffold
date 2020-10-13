c11df1b5654bfa5ee862bcb639832a663ca4e42e
tadeuasarro <tadeutas@gmail.com>
2020-10-13 18:03:31 -0300

    Rubocop run

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,5 +2,5 @@ class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
 
-  scope :to_be_confirmed, -> { where(confirmed: false)}
+  scope :to_be_confirmed, -> { where(confirmed: false) }
 end
