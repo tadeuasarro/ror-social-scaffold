d3924ffe882d84a932a2dfae58307945bdd503ec
tadeuasarro <tadeutas@gmail.com>
2020-10-14 13:59:17 -0300

    Add user model testcases

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe User do
+
+  let(:subject) do
+    described_class.new(
+      name: 'example',
+      email: 'example@example.com',
+      password: '123456'
+    )
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:password) }
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+    it 'The name of the user should exist' do
+      subject.name = ''
+      expect(subject).not_to be_valid
+    end
+    it 'The email of the user should exist' do
+      subject.email = ''
+      expect(subject).not_to be_valid
+    end
+
+    describe 'Associations', type: :model do
+      it { is_expected.to have_many(:posts) }
+      it { is_expected.to have_many(:comments) }
+      it { is_expected.to have_many(:likes) }
+      it { is_expected.to have_many(:friendships) }
+    end
+
+  end
+
+end
