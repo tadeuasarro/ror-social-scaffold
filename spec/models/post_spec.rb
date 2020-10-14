312cc8c3faee847ee7ad0e1901018d0aceafb75a
tadeuasarro <tadeutas@gmail.com>
2020-10-14 14:08:23 -0300

    Fix mistypos

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Post do
-  let(:test_user){User.create(name: 'User', email: 'user@user.com', password: '123456')}
-  let(:subject){described_class.new(content: 'This is some content', user_id: test_user.id)}
+  let(:test_user) { User.create(name: 'User', email: 'user@user.com', password: '123456') }
+  let(:subject) { described_class.new(content: 'This is some content', user_id: test_user.id) }
 
   describe 'validations' do
     it 'is valid with valid attributes' do
@@ -11,8 +11,8 @@ RSpec.describe Post do
   end
 
   describe 'associations', type: :model do
-    it {is_expected.to belong_to(:user)}
-    it {is_expected.to have_many(:comments)}
-    it {is_expected.to have_many(:likes)}
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to have_many(:comments) }
+    it { is_expected.to have_many(:likes) }
   end
 end
