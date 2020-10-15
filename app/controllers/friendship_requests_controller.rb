48740aeed39f3598ba027d0afe41e049b607e197
tadeuasarro <tadeutas@gmail.com>
2020-10-15 14:46:55 -0300

    Def create and params

--- app/controllers/friendship_requests_controller.rb
+++ app/controllers/friendship_requests_controller.rb
@@ -1,6 +1,12 @@
 class FriendshipRequestsController < ApplicationController
 
   def create
-    friendship_request = FriendshipRequest.new
+    friendship_request = FriendshipRequest.new(friendship_request_params)
+  end
+
+  private
+
+  def friendship_request_params
+    params.require(:friendship_request).permit(:user_id, :friend_id)
   end
 end
