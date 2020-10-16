35f9bd536f3ea0d9d3fd3929f9b88db4148f1dc6
tadeuasarro <tadeutas@gmail.com>
2020-10-16 14:12:36 -0300

    Fix user helper asked by TSE

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,13 +1,10 @@
 module UserHelper
   def new_friendship(user)
     unless user.id == current_user.id
-      friendship = get_friendship(user)
-      link = if friendship
-               if friendship.confirmed
-                 content_tag(:small, 'Befriended already!', class: 'p-1 rounded btn-success')
-               elsif current_user.id == friendship.user_id
-                 content_tag(:small, 'Request sent!', class: 'p-1 rounded btn-warning')
-               end
+      link = if current_user.confirmed_friendships.where(friend_id: user.id).first
+               content_tag(:small, 'Befriended already!', class: 'p-1 rounded btn-success')
+             elsif current_user.pending_friendships.where(friend_id: user.id).first
+               content_tag(:small, 'Request sent!', class: 'p-1 rounded btn-warning')
              else
                capture do
                  link_to 'Add friend',
@@ -24,11 +21,5 @@ module UserHelper
              end
     end
     link
-  end
-
-  def get_friendship(user)
-    result = Friendship.find_by(user_id: current_user.id, friend_id: user.id)
-    result = Friendship.find_by(user_id: user.id, friend_id: current_user.id) if result.nil?
-    result
   end
 end
